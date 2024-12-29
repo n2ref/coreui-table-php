5e527ed814a6974f5d50cb861302fc34b4860f12
n2ref <n2ref2@gmail.com>
2024-12-29 18:19:32 +0300

    Добавлена колонка с типом DateHuman

---
+++ src/Table/Column/DateHuman.php
@@ -0,0 +1,36 @@
+<?php
+namespace CoreUI\Table\Column;
+use CoreUI\Table\Abstract;
+
+
+/**
+ *
+ */
+class DateHuman extends Abstract\Column {
+
+    /**
+     * @param string          $field
+     * @param string|null     $label
+     * @param string|int|null $width
+     */
+    public function __construct(string $field, string $label = null, string|int $width = null) {
+
+        $this->setField($field);
+        $this->setLabel($label);
+        $this->setWidth($width);
+    }
+
+
+    /**
+     * Преобразование в массив
+     * @return array
+     */
+    public function toArray(): array {
+
+        $data = parent::toArray();
+
+        $data['type'] = 'dateHuman';
+
+        return $data;
+    }
+}
